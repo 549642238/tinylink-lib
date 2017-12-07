84d15108082d60ac4c285a237ee1e78fdb93841f
zyx0129 <33273134+zyx0129@users.noreply.github.com>
2017-12-07 20:10:04 +0800

    add GROVE_MP3 lib

---
+++ 毕设/张宇轩/GROVE_MP3/examples/TL_MP3/TL_MP3.ino
@@ -0,0 +1,52 @@
+#include"TL_Libraries.h"
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(9600); 
+   TL_MP3.init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    char recvChar=0;
+    if(Serial.available())
+    {
+      recvChar = Serial.read();
+      switch (recvChar)
+      {
+        case '1':
+            TL_MP3.SpecifyMusicPlay(1);
+            Serial.println("Specify the music index to play");
+            break;
+        case '2':
+            TL_MP3.PlayPause();
+            Serial.println("Pause the MP3 player");
+            break;
+        case '3':
+            TL_MP3.PlayResume();
+            Serial.println("Resume the MP3 player");
+            break;
+        case '4':
+            TL_MP3.PlayNext();
+            Serial.println("Play the next song");
+            break;
+        case '5':
+            TL_MP3.PlayPrevious();
+            Serial.println("Play the previous song");
+            break;
+        case '6':
+            TL_MP3.PlayLoop();
+            Serial.println("Play loop for all the songs");
+            break;
+        case '7':
+            TL_MP3.IncreaseVolume();
+            Serial.println("Increase volume");
+            break;
+        case '8':
+            TL_MP3.DecreaseVolume();
+            Serial.println("Decrease volume");
+            break;
+        default:
+            break;
+     }
+    }
+}
