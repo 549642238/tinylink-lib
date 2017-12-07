dcdfe37b5cf8999fe549673a0a8613d858b29022
zyx0129 <33273134+zyx0129@users.noreply.github.com>
2017-12-07 19:17:19 +0800

    Delete Grove_Buzzer_Buzzer_Arduino_Uno.cpp

--- 毕设/张宇轩/Buzzer/Grove_Buzzer_Buzzer_Arduino_Uno.cpp
+++
@@ -1,8 +0,0 @@
-#include <Arduino.h>
-#include "TL_Config.h"
-#include "Grove_Buzzer_Buzzer_Arduino_Uno.h"
-
-Grove_Buzzer_Buzzer::Grove_Buzzer_Buzzer():Controller(BUZZER_DIGITAL_OUTPUT){
-}
-
-Grove_Buzzer_Buzzer TL_Buzzer;
