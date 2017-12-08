e4dc4a9bb3449283edc6886a30c2b8a589d67511
Sebastian Chen <30519526+csp23@users.noreply.github.com>
2017-12-08 10:15:40 +0800

    Change 2017/12/8
    adjust file name
    set TL_Fan pin in config.h
    pass offical example of TinyBLE_MPU6050
    try TL_Gyro for TinyBLE but fail

---
+++ TinyLink实训/Chen_sipei/TinyBLE/TinyLink/TL_Config.h
@@ -0,0 +1,11 @@
+#ifndef TL_CONFIG_H
+#define TL_CONFIG_H
+#include "TL_Device_ID.h"
+#define PLATFORM 5
+#define BOARD 1011
+
+#define LED MBED_LED
+#define TIME MBED_TIME
+#define BLUETOOTH MBED_BLUETOOTH
+#define GYRO MPU6050_GYRO 
+#endif
---
+++ TinyLink实训/Chen_sipei/TinyBLE/TinyLink/TL_Libraries.h
@@ -0,0 +1,38 @@
+#ifndef TL_LIBRARIES_H
+#define TL_LIBRARIES_H
+
+#include "TL_Config.h"
+
+#ifdef TIME
+	#if PLATFORM == 5 && BOARD == 1011
+		#if TIME == MBED_TIME
+			#include "Mbed_Time_Time_Mbed.h"
+		#endif
+	#endif
+#endif
+
+#ifdef LED
+	#if PLATFORM == 5 && BOARD == 1011
+		#if LED == MBED_LED
+			#include "Mbed_Led_Led_Mbed.h"
+		#endif
+	#endif
+#endif
+
+#ifdef BLUETOOTH
+	#if PLATFORM == 5 && BOARD == 1011
+		#if BLUETOOTH == MBED_BLUETOOTH
+			#include "Mbed_Bluetooth_Bluetooth_Mbed.h"
+		#endif
+	#endif
+#endif
+//add by chen sipei
+#ifdef BLUETOOTH
+	#if PLATFORM == 5 && BOARD == 1011
+		#if GYRO == MPU6050_GYRO
+			#include "MPU6050_Gyro_Gyro_TinyBLE.h"
+		#endif
+	#endif
+#endif
+
+#endif
