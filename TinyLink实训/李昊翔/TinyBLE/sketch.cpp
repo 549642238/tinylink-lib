8541c1d387a64653acb31a0ca65e94b987f356b5
victorleegithub <33368158+victorleegithub@users.noreply.github.com>
2017-12-22 18:42:48 +0800

    直接套用了API reference中的示例代码
    目前用这个代码，可以达到要求的演示效果

---
+++ TinyLink实训/李昊翔/TinyBLE/sketch.cpp
@@ -0,0 +1,20 @@
+#include "TL_Libraries.h"
+
+void setup() {
+	TL_Bluetooth.init("My device");
+}
+void loop() {
+	char buf[20];
+	if (TL_Bluetooth.recv(buf) == 0) {
+		if (strcmp(buf, "on") == 0) {
+			TL_LED.turnOn();
+			TL_Bluetooth.send("Light On");
+		}
+		else if (strcmp(buf, "off") == 0) {
+			TL_LED.turnOff();
+			TL_Bluetooth.send("Light Off");
+		}
+	}
+	TL_Time.delayMillis(1000);
+}
+
