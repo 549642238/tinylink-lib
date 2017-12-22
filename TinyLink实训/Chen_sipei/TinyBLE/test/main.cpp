baded037b5f4f8176895f3bbd586798a9edff37a
Sebastian Chen <30519526+csp23@users.noreply.github.com>
2017-12-22 12:18:23 +0800

    Add files via upload

--- TinyLink实训/Chen_sipei/TinyBLE/test/main.cpp
+++ TinyLink实训/Chen_sipei/TinyBLE/test/main.cpp
@@ -1,21 +1,14 @@
 
 #include "TL_Libraries.h"
 
-//extern void setup();
-//extern void loop();
-
 int main(void){
-	//setup();
-	// while(true){
-	// 	loop();
-	// }
+TL_Gyro.setFSR(1000);
 while(true){
 		short x,y,z;
 	TL_Gyro.read();
 	x=TL_Gyro.data_x();
 	y=TL_Gyro.data_y();
 	z=TL_Gyro.data_z();
-	
 	printf("x=%d,y=%d,z=%d \n",x,y,z);
 	wait(1);
 }
