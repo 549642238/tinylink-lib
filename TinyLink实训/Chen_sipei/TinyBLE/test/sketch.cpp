2cf42fddb8902d1b58ec2b11f63656ae2df93425
Sebastian Chen <30519526+csp23@users.noreply.github.com>
2017-12-25 11:36:24 +0800

    test sketch

---
+++ TinyLink实训/Chen_sipei/TinyBLE/test/sketch.cpp
@@ -0,0 +1,16 @@
+#include "TL_Libraries.h"
+
+short x,y,z;
+
+void setup(){
+	TL_Gyro.setFSR(500);
+}
+
+void loop(){
+	TL_Gyro.read();
+	x=TL_Gyro.data_x();
+	y=TL_Gyro.data_y();
+	z=TL_Gyro.data_z();
+	printf("x=%d,y=%d,z=%d \n",x,y,z);
+	wait(1);
+}
