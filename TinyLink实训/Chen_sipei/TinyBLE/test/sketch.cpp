ea3af86ea9097cb0cb11dd7ff3e30927dd76678d
Sebastian Chen <30519526+csp23@users.noreply.github.com>
2017-12-27 13:50:02 +0800

    Add files via upload

--- TinyLink实训/Chen_sipei/TinyBLE/test/sketch.cpp
+++ TinyLink实训/Chen_sipei/TinyBLE/test/sketch.cpp
@@ -4,13 +4,21 @@ short x,y,z;
 
 void setup(){
 	TL_Gyro.setFSR(500);
+	
+	TL_Bluetooth.init("My device");
 }
 
 void loop(){
+	TL_LED.toggle();
 	TL_Gyro.read();
 	x=TL_Gyro.data_x();
 	y=TL_Gyro.data_y();
 	z=TL_Gyro.data_z();
-	printf("x=%d,y=%d,z=%d \n",x,y,z);
-	wait(1);
+	printf("Gyro=x=%d,y=%d,z=%d \n",x,y,z);
+	TL_Accelerometer.read(1);
+	x=TL_Accelerometer.data_x();
+	y=TL_Accelerometer.data_y();
+	z=TL_Accelerometer.data_z();
+	printf("accel=x=%d,y=%d,z=%d \n",x,y,z);
+	TL_TIME.delayMillis(5000);
 }
