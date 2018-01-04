fb4408b15f9c65235b82d711d18fbeb7cbff314c
Sebastian Chen <30519526+csp23@users.noreply.github.com>
2018-01-04 14:34:52 +0800

    Update Mbed_MPU6050_Accelerometer_Mbed.h

--- TinyLink实训/Chen_sipei/TinyBLE/TL_Accelerometer/Mbed_MPU6050_Accelerometer_Mbed.h
+++ TinyLink实训/Chen_sipei/TinyBLE/TL_Accelerometer/Mbed_MPU6050_Accelerometer_Mbed.h
@@ -1,6 +1,6 @@
 #ifndef MBED_MPU6050_MPU6050_MBED_H
 #define MBED_MPU6050_MPU6050_MBED_H
-//Main code Refer to Chenchen
+
 #include <cstdio>
 #include <cstdlib>
 #include "mbed.h"
@@ -18,37 +18,14 @@
 #define MPU6050_SDA p12
 #define MPU6050_SCL p13
 
-/* Starting sampling rate. */
-#define DEFAULT_MPU_HZ  (100)
-static signed char board_orientation[9] = {
-    1, 0, 0,
-    0, 1, 0,
-    0, 0, 1
-};
-#define q30 1073741824
-//返回数据类型
-typedef struct
-{
-    public:
-	short accel[3];//加速度
-	short gyro[3];
-	int state;
-	unsigned char new_gyo;//1-数据有效
-    unsigned char new_accel;//1-数据有效
-    //想要获取四元数和欧拉角pitch,yaw,roll，Dmp_En =1
-    long quat[4];
-    unsigned char new_angle;
-    float pitch;//围绕X轴旋转，也叫做俯仰角
-    float yaw;//围绕Y轴旋转，也叫偏航角
-    float roll;//围绕Z轴旋转，也叫翻滚角
-    unsigned char more;//1-还有数据需要读
-    short sensors;//传感器数据是否有效,&INV_XYZ_GYRO,&INV_XYZ_ACCEL,&INV_WXYZ_QUAT
-    unsigned char dmp_en;
-}AccData_Typedef;
-
-//static unsigned short inv_orientation_matrix_to_scalar(const signed char *mtx);
+#define DEFAULT_MPU_HZ (100)
 
 class Mbed_MPU6050_Accelerometer{
+private:
+	unsigned long sensor_timestamp;
+	short accel[3];
+	int state;
+	unsigned short accel_fsr,accel_rate;
 public:
     /*初始化加速度传感器
     sample_rate:采样频率 范围4HZ~1KHZ
@@ -58,18 +35,18 @@ public:
     返回0，初始化成功
     1-传感器初始化失败
     */
-	int init(unsigned short sample_rate,unsigned char accel_fsr,
-        unsigned short gyro_fsr,unsigned char Dmp_En);
+	Mbed_MPU6050_Accelerometer();
+	int setFSR(unsigned short fsr);
     /*
     Dmp_En:1-使能DMP，直接获得姿态信息，返回四元数
     */
-    AccData_Typedef read(uint8_t Dmp_En);
+    //AccData_Typedef read(uint8_t Dmp_En);
+	
+	int read();
     double data_x();
 	double data_y();
 	double data_z();
-	short x,y,z;
 };
-extern AccData_Typedef mAccData;
 
 extern Mbed_MPU6050_Accelerometer TL_Accelerometer;
 
