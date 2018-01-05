349c55c25011d5c795127d0bcfe7d68e68b3fdfc
Owen <yan13588202450@163.com>
2018-01-05 14:02:32 +0800

    1.5

---
+++ TinyLink实训/Chen_sipei/TinyBLE/test/unit test/Accelerometer_test.cpp
@@ -0,0 +1,23 @@
+#include "TL_Libraries.h"
+// #define Mbed_Led_Template
+// #define Mbed_Ble_Led_Template
+// #define Mbed_Button_Template
+//波特率9600
+#define UART_TX     p9
+#define UART_RX     p11
+Serial pc(UART_TX, UART_RX);
+void setup()
+{
+    TL_Accelerometer.setFSR(2);
+}
+
+void loop()
+{
+    short x, y , z;
+	TL_Accelerometer.read();
+	x = TL_Accelerometer.data_x();
+	y = TL_Accelerometer.data_y();
+	z = TL_Accelerometer.data_z();
+	pc.printf("x=%d y=%d z=%d\n",x,y,z);
+	wait(2);
+}
