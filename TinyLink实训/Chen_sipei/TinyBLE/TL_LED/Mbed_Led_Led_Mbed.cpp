465cd8c235750e3f1b6db22dacddb9ed4eabd242
Sebastian Chen <30519526+csp23@users.noreply.github.com>
2018-01-13 14:34:33 +0800

    增加了State函数

--- TinyLink实训/Chen_sipei/TinyBLE/TL_LED/Mbed_Led_Led_Mbed.cpp
+++ TinyLink实训/Chen_sipei/TinyBLE/TL_LED/Mbed_Led_Led_Mbed.cpp
@@ -2,14 +2,20 @@
 #include <stdlib.h>
 #include "Mbed_Led_Led_Mbed.h"
 
+
         void Mbed_Led_Led_T::turnOff(){
 				led = 1;
+				stateSig = 0;
         }
         void Mbed_Led_Led_T::turnOn(){
 				led = 0;
-        }
+				stateSig = 1;        
+		}
         void Mbed_Led_Led_T::toggle(){
-              led = !led;
+              led = !led ; 
+        }
+        int Mbed_Led_Led_T::state(){
+      		return stateSig;
         }
 
 Mbed_Led_Led TL_LED;
