4628ff7f3c8d61f453a0210ef823fd182e42336e
Sebastian Chen <30519526+csp23@users.noreply.github.com>
2018-01-15 15:45:41 +0800

    Update Gyro_test.cpp

--- TinyLink实训/Chen_sipei/TinyBLE/test/unit test/Gyro_test.cpp
+++ TinyLink实训/Chen_sipei/TinyBLE/test/unit test/Gyro_test.cpp
@@ -1,5 +1,7 @@
 #include "TL_Libraries.h"
-
+#define UART_TX     p9
+#define UART_RX     p11
+Serial pc(UART_TX, UART_RX,9600);
 short x,y,z;
 
 void setup(){
@@ -11,6 +13,6 @@ void loop(){
 	x=TL_Gyro.data_x();
 	y=TL_Gyro.data_y();
 	z=TL_Gyro.data_z();
-	printf("x=%d,y=%d,z=%d \n",x,y,z);
+	pc.printf("x=%d,y=%d,z=%d \n",x,y,z);
 	wait(2);
 }
